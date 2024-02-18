f9997d932bbf324473ef256dfdef5ebe3fbdc9cc
karanlongani <karanlongani8@gmail.com>
2024-02-18 12:29:10 +0530

    url feature added , secind api integrated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,83 +1,135 @@
 import requests
 import json
 import streamlit as st
-import base64
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
 
-# Define a function to display the product page
+
 def display_product_page(image_url, product_info):
-    # Set title and page layout
     st.title("1000340-35-1")
-    bin_str = ""
-    
-    st.markdown(
-        """
-        <style>
-        /* Add a background with a chemistry-themed image */
-        body {
-            background-image: url("data:image/png;base64,%s");
-            background-size: cover;
-            background-repeat: no-repeat;
-            font-family: Arial, sans-serif;
-        }
-        /* Style for bold key in JSON */
-        .json-key {
-            font-weight: bold;
-            color: #0066ff; /* Blue color for keys */
-        }
-        </style>
-        """% bin_str,
-        unsafe_allow_html=True,
-    )
+    page_bg_img = f"""
+    <style>
 
-    # Use Streamlit columns for layout
+    body {{
+        font-family: Arial, sans-serif;
+    }}
+
+    .json-key {{
+        font-weight: bold;
+        color: #0066ff; 
+    }}
+
+    </style>
+    """
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
     col1, col2 = st.columns([2, 3])
 
-    # Display image on the left side
     with col1:
         st.image(image_url, use_column_width=True)
 
-    # Display product information on the right side
     with col2:
         st.markdown("**Description**")
-        # Iterate over product info and display key-value pairs
         for key, value in product_info.items():
             st.markdown(f"<span class='json-key'>{key}:</span> {value}", unsafe_allow_html=True)
 
-# Sample product information
-product_info = {
-    "Catalog Number": "24729564",
-    "Chemical Name": "4-Bromo-1H-pyrrolo[2,3-B]pyridine-3-carbaldehyde",
-    "CAS Number": "1000340-35-1",
-    "Molecular Formula": "C8H5BrN2O",
-    "Molecular Weight": "225.0421"
-}
 
-# URL of the product image
-product_image_url = "https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?sid=391946015&t=s&deposited=t&version=1"
+def find_dictonary(list_of_dictonary,key_,value_):
+    for dic in list_of_dictonary:
+        value_from_dic = dic.get(key_,"")
 
-# Display the product page
-display_product_page(product_image_url, product_info)
+        if value_ == value_from_dic:
+            return dic
+        
+    else:
 
-# response = requests.request("GET", url)
+        return {}
+    
+def get_cid(cas_no):
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/concepts/name/JSON?name={cas_no}"
+    print(url)
+    response = requests.request("GET", url)
+    response = response.text
+    response = json.loads(response)
 
-# response = response.text
-# response = json.loads(response)
+    cid_no = response["ConceptsAndCIDs"]["CID"][0]
+    print(cid_no)
 
-# chemical_name = response["Record"]["RecordTitle"]
-# print(chemical_name)
-# cas_number = response["Record"]["Reference"][0]["SourceID"]
+    return cid_no
+
+    
+def product(ref_id):
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{ref_id}/JSON/"
+    response = requests.request("GET", url)
+    response = response.text
+    response = json.loads(response)
+    # print(response)
+
+    chemical_name = response["Record"]["RecordTitle"]
+    cas_number = response["Record"]["Reference"][0]["SourceID"]
+
+    mf = response["Record"]["Section"]
+    mf = find_dictonary(mf,"TOCHeading","Names and Identifiers")
+    mf = find_dictonary(mf["Section"],"TOCHeading","Molecular Formula")
+    # print(mf)
+    mf = mf["Information"][0]
+    # print(mf)
+    mf = mf["Value"]["StringWithMarkup"][0]["String"]
+    print(mf)
+
+    mw = response["Record"]["Section"]
+    mw = find_dictonary(mw , "TOCHeading" , "Chemical and Physical Properties")
+    mw = find_dictonary(mw["Section"] , "TOCHeading" , "Computed Properties")
+    mw = find_dictonary(mw["Section"],"TOCHeading","Molecular Weight")
+    mw = mw["Information"][0] 
+    mw = mw["Value"]["StringWithMarkup"][0]["String"]
+    print(mw)
+
+    return [chemical_name , cas_number , mf , mw]
+
+
+def pc_to_cas(pc_num:str):
+
+    return "3600-87-1"
+
+params = st.experimental_get_query_params()
+pc_num = params.get('pc_num', [None])[0]
+
+print("Parameter value:", pc_num)
+
+
+if pc_num:
+
+    cas_num = pc_to_cas(pc_num=pc_num)
+    print(cas_num)
+    cid = get_cid(cas_no=cas_num)
+    details = product(cid)
+
+
+    product_info = {
+        "Catalog Number": cid,
+        "Chemical Name": details[0],
+        "CAS Number": details[1],
+        "Molecular Formula": details[2],
+        "Molecular Weight": details[3]
+    }
+
+    product_image_url = f"https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid={cid}&t=l"
+
+    display_product_page(product_image_url, product_info)
+
+
+
+    
 
 
 
 
-# print(cas_number)
+    
 
-# st.json(response)
+
+
+
+
 
 
 
